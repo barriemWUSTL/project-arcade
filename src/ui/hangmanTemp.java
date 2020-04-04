b88557adff0e4b37ccd0508f846b32d423ef6035
Kati Gabbard <katherine.kate@wustl.edu>
2020-04-03 20:11:09 -0500

    finished preliminary launcher

---
+++ src/ui/hangmanTemp.java
@@ -0,0 +1,5 @@
+public class hangmanTemp {
+	public static void run() {
+		System.out.println("The hangman file has run");
+	}
+}
--- src/ui/ui.java
+++ src/ui/ui.java
@@ -1,4 +1,3 @@
-import java.io.File;
 import java.util.Scanner;
 
 public class ui {			
@@ -7,24 +6,58 @@ public class ui {
 				"Select 1 or 2, and then press enter to play!" + "\n" +
 				"Press 1 for Hangman" + "\n" + "Press 2 for War");    	
 		Scanner scanner = new Scanner(System.in);
-		int input = scanner.nextInt();	 //add a try catch to deal with the invalid input exception
-		if(input == 1) {
-			System.out.println("Hangman selected");
-			File file = new File("../hangman/hangman.java");
-	        if(file.exists()) {
-	        	//desktop.open(file);
-	        }
+		try { 
+			boolean badInput = true;
+			while(badInput) { //if there is bad input, run through the loop again
+				int input = scanner.nextInt();
+				if(validInput(input)) {
+					badInput = false;
+					if(input == 1) {
+						System.out.println("Hangman selected");
+						runfile(input);
+					}
+					else if(input == 2) {
+						System.out.println("War selected");
+						runfile(input);
+					}
+				}
+				else {
+					System.out.println("Invalid input. Please select 1 or 2");
+					input = scanner.nextInt();
+					System.out.println(input);
+				
+				}				
+			}
+			
 		}
-		else if(input == 2) {
-			System.out.println("War selected");
-		}
-		else {
+		catch (java.util.InputMismatchException e) {
 			System.out.println("Invalid input. Please select 1 or 2");
-			input = scanner.nextInt();
-			System.out.println(input);
 		}
 		scanner.close();
-		
-		
 		}
+	
+	/**
+	 * returns true once the input is 1 or 2
+	 * @param input
+	 * @return
+	 */
+	private static boolean validInput(int input) {
+		if(input == 1 || input == 2) {
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public static void runfile(int input) {
+		if (input == 1) {
+			hangmanTemp.run();
+			//call method to start hangman
+		}
+		else if (input ==2 ) {
+			warTemp.run();
+			//call method to start war
+		}
+	}
 }
