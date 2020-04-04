fbc711baac7017cfedc06bc6b601ea6394919508
Rachana Reddy <rachanaereddy@gmail.com>
2020-04-03 19:38:42 -0500

    more edits

--- src/arcade/hangman.java
+++ src/arcade/hangman.java
@@ -7,7 +7,7 @@ public class Hangman {
     public static String[] easyWords = {"apple", "pear", "cherry", "noodle", "bread", "pasta", "chip", "salsa", "milk", "cookie"};
     public static String[] hardWords = {"avocado", "sandwich", "carrots", "cucumber", "strawberry", "artichoke", "watermelon", "croissant", "chicken", "ravioli"};
 
-    public static int wrongGuesses = 3;
+    public static int wrongGuesses = 0;
 
     public static boolean isWon = false;
 
@@ -39,49 +39,78 @@ public class Hangman {
         }else if(numWrong ==6){
             printable = "\t O\t\n\t/|\\\n\t/ \\\n YOU LOST\n";
         }
+        System.out.println("_________");
         System.out.print(printable);
+        System.out.println();
     }
 
-    public static void printGame() {
+
+    public static boolean play() {
         String word = selectWord();
         System.out.println("Selected word: " + word);
-        String[] answer = new String[word.length()];
+        char[] answer = word.toCharArray(); //char array that is the goal word
+        char[] guess = new char[answer.length];
 
-        for (int i = 0; i < answer.length; i++) {
+        System.out.print("Guess: ");
+        for (int i = 0; i < guess.length; i++) {
             System.out.print("_ ");
         }
+        System.out.println();
+
+        while(wrongGuesses <= 6 || !isWon) {
+            Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Please enter a letter.");
+            char letterInput = scanner.nextLine().charAt(0);  // Read user input
+            System.out.println(letterInput);
+
+            int wrongCounter = 0;
+            for(int i=0; i<answer.length; i++) {
+                if(letterInput == answer[i]) {
+                    guess[i] = letterInput;
+                } else {
+                    if(!Character.isLetter(guess[i])) {
+                        guess[i] = '_';
+                        wrongCounter++;
+                    }
+                }
+            }
+            if (wrongCounter == answer.length) {
+                wrongGuesses++;
+                if (wrongGuesses == 6) {
+                    System.out.println("YOU LOST");
+                    drawHangman(wrongGuesses);
+                } else {
+                    System.out.println("Input is incorrect. Guesses remaining: " + (6-wrongGuesses));
+                    drawHangman(wrongGuesses);
+                }
+            }
+
+            int winCounter = 0;
+            for (int i = 0; i<answer.length;i++) {
+                if(answer[i] == guess[i]) {
+                       winCounter++;
+                }
+            }
+            if (winCounter == answer.length) {
+                System.out.println("YOU WON");
+                isWon = true;
+            }
+
+            System.out.print("New Guess: ");
+            for (int i = 0; i < guess.length; i++) {
+                System.out.print(guess[i]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        return false;
     }
 
 
 
     public static void main(String[] args) {
-//        if (difficultySelection.equals("easy")) { //if user entered easy
-//            System.out.println("Easy selected.");
-//            int randomIndex = (int)(Math.random()*easyWords.length);
-//            System.out.println("Selected: " + easyWords[randomIndex]);
-//            System.out.println("Word length: " + easyWords[randomIndex].length());
-//
-//            String word = easyWords[randomIndex];
-//            String[] answer = new String[easyWords[randomIndex].length()];
-//
-//            for (int i = 0; i < answer.length; i++) {
-//                System.out.print("_ ");
-//
-//            }
-//            System.out.println();
-//
-//            while (wrongGuesses != 6) {
-//
-//            }
-//
-//
-//        } else {
-//            System.out.println("Hard selected."); //if user entered hard
-//            int randomIndex = (int)(Math.random()*hardWords.length);
-//            System.out.println("Selected:" + hardWords[randomIndex]);
-//        }
-
-
+        play();
 
     }
 }
